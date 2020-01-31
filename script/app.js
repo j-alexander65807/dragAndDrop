dcaf6d2aaf03d1f9098bc8a96d6587e1aa61c1d6
Alexander <j_alexander65807@ats-hr420-dm33.fc.ca>
2020-01-31 14:41:54 -0500

    kinda working

--- script/app.js
+++ script/app.js
@@ -2,17 +2,40 @@
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
 		puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+		dropZones = document.querySelectorAll('.drop-zone'),
 				gameBoard = document.querySelector('.puzzle-board');
 
 	const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 	function changeImageSet() {
 		//change all the image elements on the page -> draggable image sources,
-		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`);
+		pieceNames.forEach((piece, index) => {
+			puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`;
+			puzzlePieces[index].id = `${piece + this.dataset.puzzleref}.jpg`;
+		});
 
 		// and set the drop zone background
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
 		//debugger;
+	}
+
+	function allowDrag(event) {
+		console.log('started dragging');
+
+		event.dataTransfer.setData("text/plain", this.id);
+	}
+	function allowDragOver(event) {
+		event.preventDefault();
+		console.log('drag over');
+	}
+	function allowDrop(event) {
+
+		console.log('dropped');
+
+		let currentImage = event.dataTransfer.getData("text/plain");
+
+		event.target.appendChild(document.querySelector(`#${currentImage}`));
+
 	}
 
 
@@ -21,9 +44,14 @@
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
+	dropZones.forEach(zone => {
+		zone.addEventListener('dragover', allowDragOver);
+		zone.addEventListener('drop', allowDrop);
+	});
 
 	//calling a function to focus on load
-changeImageSet.call(puzzleButtons[0])
+changeImageSet.call(puzzleButtons[0]);
 
 })();
